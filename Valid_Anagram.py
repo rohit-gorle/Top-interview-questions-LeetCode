397db91442964093fcfc68e56d3a25a65611ed30
Rohit gorle <61993839+rohit-gorle@users.noreply.github.com>
2020-06-26 14:35:43 -0700

    Strings
    Using dictionary and .get(i,0)+1 method to count how many times the particular element occurred in the dictionary

---
+++ Valid_Anagram.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False      
+        
+        dic1 = {}
+        dic2 = {}        
+        
+        for i in s:
+            dic1[i] = dic1.get(i,0)+1
+        for i in t:
+            dic2[i] = dic2.get(i,0)+1
+            
+        return dic1==dic2    
